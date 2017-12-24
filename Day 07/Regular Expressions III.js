921e6dbd78b2c64ca3b2fa76ab4e0779c3c6673f
Youssef Ali <youssefali6212@gmail.com>
2017-12-24 11:59:19 +0200

    Create Regular Expressions III.js

---
+++ Day 07/Regular Expressions III.js
@@ -0,0 +1,13 @@
+// link : https://www.hackerrank.com/challenges/js10-regexp-3/problem
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match ALL occurrences of numbers in a string.
+     */
+    var re = /\d+/g;
+    
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
