a1377da2c9f48caa4da5cad665138517d830fe7b
Youssef Ali <youssefali6212@gmail.com>
2017-12-24 13:33:33 +0200

    Create Inheritance.js

---
+++ Day 05/Inheritance.js
@@ -0,0 +1,16 @@
+// link : https://www.hackerrank.com/challenges/js10-inheritance/problem
+/*
+ *  Write code that adds an 'area' method to the Rectangle class' prototype
+ */
+    Rectangle.prototype.area = function(){
+        return this.w * this.h;
+    };
+/*
+ * Create a Square class that inherits from Rectangle and implement its class constructor
+ */
+
+    class Square extends Rectangle {
+        constructor(l) {
+            super(l,l);
+        }
+    }
