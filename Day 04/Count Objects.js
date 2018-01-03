be7902efef83320448654eea2fdfc9004235e3aa
Youssef Ali <youssefali6212@gmail.com>
2018-01-03 21:37:25 +0200

    Create Count Objects.js

---
+++ Day 04/Count Objects.js
@@ -0,0 +1,12 @@
+//link: https://www.hackerrank.com/challenges/js10-count-objects/problem
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+    return objects.filter(function(o){
+        return o.x == o.y;
+    }).length;
+}
