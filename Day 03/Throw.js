97f8c3c9d65586e4a552ebbb9da3b0b192e275f9
Youssef Ali <youssefali6212@gmail.com>
2018-01-08 11:55:08 +0200

    Create Throw.js

---
+++ Day 03/Throw.js
@@ -0,0 +1,15 @@
+//link: https://www.hackerrank.com/challenges/js10-throw/topics/javascript-errors
+/*
+ * Complete the isPositive function.
+ * If 'a' is positive, return "YES".
+ * If 'a' is 0, throw an Error with the message "Zero Error"
+ * If 'a' is negative, throw an Error with the message "Negative Error"
+ */
+function isPositive(a) {
+    if(a > 0)
+        return "YES";
+    else if(a == 0)
+        throw new Error("Zero Error");
+    else
+        throw new Error("Negative Error");
+}
