02a7041f1d5132f47a4ec69ed3f2ce368e4b7456
Youssef Ali <youssefali6212@gmail.com>
2020-06-03 16:28:43 +0200

    Update TemplateLiterals.js

--- Day 05/TemplateLiterals.js
+++ Day 05/TemplateLiterals.js
@@ -13,5 +13,5 @@ function sides(literals, ...expressions) {
     const [a, p] = expressions;
     const sqrt = Math.sqrt(p ** 2 - 16 * a);
 
-    return [(p + sqrt) / 4, (p - sqrt) / 4];
+    return [(p - sqrt) / 4, (p + sqrt) / 4];
 }
