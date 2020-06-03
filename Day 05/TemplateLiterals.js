7e1e84aa5e6cf08866ac25deb9b0d55868af51c1
Youssef Ali <youssefali6212@gmail.com>
2020-06-03 16:24:29 +0200

    Create TemplateLiterals.js

---
+++ Day 05/TemplateLiterals.js
@@ -0,0 +1,18 @@
+//link: https://www.hackerrank.com/challenges/js10-template-literals/problem
+
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ * 
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+function sides(literals, ...expressions) {
+    const a = expressions[0];
+    const p = expressions[1];
+    const sqrt = Math.sqrt(p ** 2 - 16 * a);
+
+    return [(p + sqrt) / 4, (p - sqrt) / 4].sort();
+}
